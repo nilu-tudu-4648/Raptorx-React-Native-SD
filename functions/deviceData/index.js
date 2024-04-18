fd6028f0426d857614b0749aff3d137daf4cbe30
tudunilesh3@gmail.com <nilesh.tudu@nirmitee.io>
2024-04-18 12:19:46 +0530

    device data fixed

--- functions/deviceData/index.js
+++ functions/deviceData/index.js
@@ -2,6 +2,7 @@ import * as RNLocalize from "react-native-localize";
 import DeviceInfo from "react-native-device-info";
 import { bytesToMB } from "../../utils/raptorx-utils";
 import RNFS from "react-native-fs";
+import { Dimensions } from "react-native";
 
 export const makeDeviceDataApiCall = async (
   api,
@@ -9,66 +10,55 @@ export const makeDeviceDataApiCall = async (
   customerId,
   deviceData
 ) => {
-  const { ip_address, device_id, is_emulator,
-     device_model,os_name,os_version ,available_storage} =
-    deviceData;
-  const params = { url: "api/analytics/device/capture" };
   try {
-    const postData = {
+    const response = await api.post("api/analytics/device/capture", {
       session_id: sessionId,
       customer_id: customerId,
-      device_id,
-      ip_address,
-      device_model,
-      is_emulator,
-      available_storage,
-      os_name,
-      os_version
-    };
-    const deviceDataResponse = await api.post(params.url, postData);
-    console.log({deviceDataResponse})
-    return deviceDataResponse;
+      ...deviceData
+    });
+    console.log("Device data API response:", response);
+    return response.data; // Assuming the API response contains useful data
   } catch (error) {
-    console.error("Error making API call:");
+    console.error("Error making device data API call:", error);
     throw error;
   }
 };
+
 const getAllDeviceData = async (api, sessionId, customerId) => {
   try {
     const uniqueId = await DeviceInfo.getUniqueId();
     const manufacturer = await DeviceInfo.getManufacturer();
     const carrier = await DeviceInfo.getCarrier();
-    const brand = DeviceInfo.getBrand();
+    const deviceVendor = DeviceInfo.getBrand();
     const device_model = DeviceInfo.getModel();
     const is_emulator = await DeviceInfo.isEmulator();
-    const device_id = DeviceInfo.getDeviceId(); // Retrieve deviceId directly
+    const device_id = DeviceInfo.getDeviceId();
     const os_name = DeviceInfo.getSystemName();
     const os_version = DeviceInfo.getSystemVersion();
     const buildId = await DeviceInfo.getBuildId();
     const ip_address = await DeviceInfo.getIpAddress();
     const instanceId = await DeviceInfo.getInstanceId();
     const deviceName = await DeviceInfo.getDeviceName();
-    const userAgent = await DeviceInfo.getUserAgent();
-    const apiLevel = await DeviceInfo.getApiLevel();
-    const bootloader = await DeviceInfo.getBootloader();
+    const userAgent = await DeviceInfo.getUserAgent();//
     const baseOs = await DeviceInfo.getBaseOs();
-    const fingerprint = await DeviceInfo.getFingerprint();
-    const tags = await DeviceInfo.getTags();
-    const type = await DeviceInfo.getType();
-    const buildNumber = DeviceInfo.getBuildNumber();
-    const bundleId = DeviceInfo.getBundleId();
-    const appName = DeviceInfo.getApplicationName();
-    const version = DeviceInfo.getVersion();
-    const readableVersion = DeviceInfo.getReadableVersion();
+    const fingerprint = await DeviceInfo.getFingerprint();//
+    const buildNumber = await DeviceInfo.getBuildNumber();
+    const bundleId = await DeviceInfo.getBundleId();
+    const appName = await DeviceInfo.getApplicationName();
+    const version = await DeviceInfo.getVersion();
+    const readableVersion = await DeviceInfo.getReadableVersion();
+    const deviceRam = DeviceInfo.getTotalMemory();
     const localLanguage = RNLocalize.getLocales()[0].languageCode;
     const storageInfo = await RNFS.getFSInfo();
     const available_storage = bytesToMB(storageInfo.freeSpace);
-
+    const { width, height } = Dimensions.get("window");
+    const screenResolution = `${width}x${height}`;
+    
     const deviceData = {
       uniqueId,
       manufacturer,
       carrier,
-      brand,
+      deviceVendor,
       device_model,
       is_emulator,
       device_id,
@@ -79,12 +69,8 @@ const getAllDeviceData = async (api, sessionId, customerId) => {
       instanceId,
       deviceName,
       userAgent,
-      apiLevel,
-      bootloader,
       baseOs,
       fingerprint,
-      tags,
-      type,
       buildNumber,
       bundleId,
       appName,
@@ -92,7 +78,10 @@ const getAllDeviceData = async (api, sessionId, customerId) => {
       readableVersion,
       localLanguage,
       available_storage,
+      screenResolution,
+      deviceRam
     };
+    
     const deviceDataResult = await makeDeviceDataApiCall(
       api,
       sessionId,
